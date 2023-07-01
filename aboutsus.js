add12094430bb0b7a70e8f6529b35c07113b1c36
Shiv Shikhar Sinha <shiv.shikhar.7@gmail.com>
2023-07-01 14:14:02 +0530

    Modifies About us Page

--- aboutsus.js
+++ aboutsus.js
@@ -18,21 +18,65 @@ menu.onclick = function(){
     }
 }
 
-const counts = document.querySelectorAll('.count')
-const speed = 12
-counts.forEach((counter) =>{
-    // root: null,
-    // threshold: 0,
-    function upData(){
-        const target = Number(counter.getAttribute('data-target'))
-        const count = Number(counter.innerText)
-        const inc = target / speed
-        if (count < target){
-            counter.innerText = Math.floor(inc + count)
-            setTimeout(upData, 1)
-        } else{
-            counter.innerText = target
-        }
+// OLD JS ANIMATION CODE
+
+// const counts = document.querySelectorAll('.count')
+// const speed = 12
+// counts.forEach((counter) =>{
+//     // root: null,
+//     // threshold: 0,
+//     function upData(){
+//         const target = Number(counter.getAttribute('data-target'))
+//         const count = Number(counter.innerText)
+//         const inc = target / speed
+//         if (count < target){
+//             counter.innerText = Math.floor(inc + count)
+//             setTimeout(upData, 1)
+//         } else{
+//             counter.innerText = target
+//         }
+//     }
+//     upData()
+// })
+
+// OLD CODE IMPROVED
+const counters = document.querySelectorAll('.count');
+const speed = 69;
+
+function animateCounters() {
+  counters.forEach(counter => {
+    const target = +counter.getAttribute('data-target');
+    const increment = target / speed;
+    let count = 0;
+
+    const updateCounter = () => {
+      if (count < target) {
+        counter.innerText = Math.floor(count);
+        count += increment;
+        requestAnimationFrame(updateCounter);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+const options = {
+  root: null,
+  threshold: 0.9, // Adjust the threshold as needed
+};
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      animateCounters();
+      observer.unobserve(entry.target);
     }
-    upData()
-})
+  });
+}, options);
+
+counters.forEach(counter => {
+  observer.observe(counter);
+});
